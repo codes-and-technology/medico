36e65a2661fde1e01364c199cd0b70da41eb9f57
daniel <contato.danielharo@gmail.com>
2025-01-28 23:27:17 -0300

    correção dos testes unitários

--- create-user-api/tests/CreateUserTests/CreateUserTests.cs
+++ create-user-api/tests/CreateUserTests/CreateUserTests.cs
@@ -1,4 +1,6 @@
+using System.Linq.Expressions;
 using CreateController;
+using CreateEntitys;
 using CreateInterface;
 using Moq;
 using Presenters;
@@ -11,12 +13,12 @@ namespace UserApiCreateTests
         [InlineData("usuario teste", "40993229808", "teste@teste.com")]
         public async Task When_CreateContact_ShouldBe_Ok(string name, string document, string email)
         {
-            var mockIUserQueueGateway = new Mock<IUserQueueGateway>();
-            var mockIUserConsultingGateway = new Mock<IUserConsultingGateway>();
+            var userDbGateway = new Mock<IUserDBGateway>();
+            var authDbGateway = new Mock<IAuthDBGateway>();
 
-            mockIUserConsultingGateway.Setup(s => s.GetUser(It.IsAny<string>())).ReturnsAsync(new UserConsultingDto());
-
-            var createContactController = new CreateUserController(mockIUserConsultingGateway.Object, mockIUserQueueGateway.Object);
+            userDbGateway.Setup(s => s.FirstOrDefaultAsync(It.IsAny<Expression<Func<UserEntity, bool>>>()))
+                .ReturnsAsync(null as UserEntity);
+            var createContactController = new CreateUserController(userDbGateway.Object, authDbGateway.Object);
 
             UserDto dto = new()
             {
@@ -25,7 +27,7 @@ namespace UserApiCreateTests
                 DocumentNumber = document
             };
 
-            var result = await createContactController.CreateUserAsync(dto, Presenters.Enum.UserType.Patient, null);
+            var result = await createContactController.CreateUserAsync(dto);
 
             Assert.NotNull(result);
         }
@@ -34,18 +36,16 @@ namespace UserApiCreateTests
         [InlineData("usuario teste", "988027555", "teste@teste.com")]
         public async Task When_CreateContact_ShouldBe_Error_WithContactExists(string name, string document, string email)
         {
-            var mockIUserQueueGateway = new Mock<IUserQueueGateway>();
-            var mockIUserConsultingGateway = new Mock<IUserConsultingGateway>();
+            var userDbGateway = new Mock<IUserDBGateway>();
+            var authDbGateway = new Mock<IAuthDBGateway>();
 
-
-            mockIUserConsultingGateway.Setup(s => s.GetUser(It.IsAny<string>())).ReturnsAsync(new UserConsultingDto
-            {
-                Email = email,
-                Id = Guid.NewGuid().ToString(),
-                Name = name,
-            });
-
-            var createContactController = new CreateUserController(mockIUserConsultingGateway.Object, mockIUserQueueGateway.Object);
+            userDbGateway.Setup(s => s.FirstOrDefaultAsync(It.IsAny<Expression<Func<UserEntity, bool>>>()))
+                .ReturnsAsync(new UserEntity
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Email = email
+                });
+            var createContactController = new CreateUserController(userDbGateway.Object, authDbGateway.Object);
 
             UserDto dto = new()
             {
@@ -54,44 +54,24 @@ namespace UserApiCreateTests
                 DocumentNumber = document
             };
 
-            var result = await createContactController.CreateUserAsync(dto, Presenters.Enum.UserType.Doctor, 13253);
+            var result = await createContactController.CreateUserAsync(dto);
 
             Assert.False(result.Success);
         }
 
-        [Theory]
-        [InlineData("usuario teste", "988027555", "teste@teste.com")]
-        public async Task When_CreateContact_ShouldBe_Error_ApiConsulting(string name, string document, string email)
-        {
-            var mockIUserQueueGateway = new Mock<IUserQueueGateway>();
-            var mockIUserConsultingGateway = new Mock<IUserConsultingGateway>();
-
-            mockIUserConsultingGateway.Setup(s => s.GetUser(It.IsAny<string>())).ThrowsAsync(new Exception("Falha ao tentar consultar usuário"));
-
-            var createContactController = new CreateUserController(mockIUserConsultingGateway.Object, mockIUserQueueGateway.Object);
-
-            UserDto dto = new()
-            {
-                Name = name,
-                Email = email,
-                DocumentNumber = document,
-            };            
-
-            await Assert.ThrowsAnyAsync<Exception>(async () => await createContactController.CreateUserAsync(dto,Presenters.Enum.UserType.Doctor, 12345));
-        }
-
+        
         [Theory]
         [InlineData("usuario teste", "40993229808", "")]
         [InlineData("usuario teste", "", "")]
         [InlineData("", "57829013457082349", "teste@teste.com")]
         public async Task When_CreateContact_ShouldBe_Error_WithInvalidDto(string name, string document, string email)
         {
-            var mockIUserQueueGateway = new Mock<IUserQueueGateway>();
-            var mockIUserConsultingGateway = new Mock<IUserConsultingGateway>();
+            var userDbGateway = new Mock<IUserDBGateway>();
+            var authDbGateway = new Mock<IAuthDBGateway>();
 
-            mockIUserConsultingGateway.Setup(s => s.GetUser(It.IsAny<string>())).ReturnsAsync(new UserConsultingDto { });
-
-            var createContactController = new CreateUserController(mockIUserConsultingGateway.Object, mockIUserQueueGateway.Object);
+            userDbGateway.Setup(s => s.FirstOrDefaultAsync(It.IsAny<Expression<Func<UserEntity, bool>>>()))
+                .ReturnsAsync(null as UserEntity);
+            var createContactController = new CreateUserController(userDbGateway.Object, authDbGateway.Object);
 
             UserDto dto = new()
             {
@@ -100,7 +80,7 @@ namespace UserApiCreateTests
                 DocumentNumber= document,
             };
 
-            var result = await createContactController.CreateUserAsync(dto, Presenters.Enum.UserType.Patient, null);
+            var result = await createContactController.CreateUserAsync(dto);
 
             Assert.False(result.Success);
         }
